330025ca6e05f630aac3f02fd992725d7e26bd08
Farhana Mahmud <farhana_mahmud@outlook.com>
2023-05-28 01:20:42 -0400

    4th Commit:(Lab2) Followed the old process of importing widgets into Java.

--- app/src/main/java/algonquin/cst2335/mahm008887/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mahm008887/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.mahm008887;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView theText = findViewById(R.id.mytext);
+        Button theButton = findViewById(R.id.mybutton);
+        EditText theEdit = findViewById(R.id.myedit);
+
+        String editString = theEdit.getText().toString();
+        theText.setText( "Your edit text has: " + editString);
+
+
     }
 }
