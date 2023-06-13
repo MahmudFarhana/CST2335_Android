c723e7200f64125dc165ab5cb3b0b273bccce0a0
Farhana Mahmud <farhana_mahmud@outlook.com>
2023-06-13 13:34:37 -0400

    9th Commit:(Lab4) Final version of Week4

--- app/src/main/java/algonquin/cst2335/mahm008887/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mahm008887/MainActivity.java
@@ -1,38 +1,84 @@
 package algonquin.cst2335.mahm008887;
 
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import algonquin.cst2335.mahm008887.databinding.ActivityMainBinding;
 
+//import com.example.FARHANAsA.databinding.ActivityMainBinding;
+
+
+
 public class MainActivity extends AppCompatActivity {
-    private static String TAG = "MainActivity";
+
+    private  String TAG = "MainActivity";
     protected ActivityMainBinding binding;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Log.w( "MainActivity", "The application is now visible on screen." );
-
-    }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.w( "MainActivity", "The application is now responding to user input" );
-       // Log.d( TAG, "Message");
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.w( "MainActivity", "The application no longer responds to user input" );
+        setContentView(binding.getRoot());
+
+
+
+
+
+
+        binding.loginButton.setOnClickListener(v ->
+        {          // where to go:      leaving here going to SecondActivity
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+
+            nextPage.putExtra("EmailAddress", binding.emailText.getText().toString());
+
+
+
+            String whatIsTyped = binding.emailText.getText().toString();
+
+            nextPage.putExtra("EMAIL", whatIsTyped);
+            nextPage.putExtra("AGE", 25);
+            nextPage.putExtra("DAY", "Tuesday");
+
+//            Intent intent = new Intent(Intent.ACTION_DIAL);
+//            intent.setData(Uri.parse("tel: " + "6137274723"));
+//
+////            intent.setType("*/*");
+////            intent.putExtra(Intent.EXTRA_EMAIL, new String [] {"A@A.COM", "B2B.COM"});
+////            intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+////            intent.setType("*/*");
+////            intent.putExtra(Intent.EXTRA_EMAIL, new String [] {"A@A.COM", "B2B.COM"});
+////            intent.putExtra(Intent.EXTRA_SUBJECT, "Subject");
+//            startActivity( intent);
+            startActivity( nextPage);
+
+        });
+        // Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Log.e(TAG, "you clicked the button");
+
+
+
+
+//
+
+
+
+
+        //goes another page
+        // startActivity(cameraIntent); //carries all the data to the next page
+
+
+
+
+        Log.w( TAG , "In onCreate() - Loading Widgets" );
+        Log.d( TAG, "Message");
 
 
     }
@@ -40,56 +86,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.w( "MainActivity", "The application is no longer visible." );
+        Log.w( TAG , "The application is no longer visible." );
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( TAG , "The application is now responding to user" );
 
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.w( "MainActivity", "Any memory used by the application is freed." );
+        Log.w( TAG , "Any memory used by the application is freed." );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( TAG , " The application no longer responds to user input" );
+
+        Log.e ( TAG, "onPause");
 
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-//        binding.loginButton.setOnClickListener( cl -> {
-//                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//                    startActivity(cameraIntent);
-//
-//                });
-
-        binding.loginButton.setOnClickListener((v) ->
-        {
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-           // Log.e(TAG, "You clicked the button");
-            //Where to go:          leaving here        going to SecondActivity
-           /* Intent nextPage = new Intent(this, SecondActivity.class);
-
-           String whatIsTyped = binding.emailText.getText().toString();
-
-
-            nextPage.putExtra("EMAIL", whatIsTyped);
-            nextPage.putExtra("AGE", 25);
-            nextPage.putExtra("DAY", "Tuesday");
-        */
-
-           // Intent intent = new Intent(Intent.ACTION_SEND);
-           // intent.setType("*/*");
-            //intent.setData(Uri.parse("https://www.algonquincollege.com"));
-           // intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"A@A.com", "B@B.com"});
-           // intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
-        //go to another page
-            startActivity(cameraIntent);//Carries all data to the next page.
-
-        });
-        Log.w("MainActivity", "In onCreate() - Loading Widgets");
-
+    protected void onStart() {
+        super.onStart();
+        Log.w( TAG , "The application is now visible on screen" );
     }
-
 }
+
--- app/src/main/java/algonquin/cst2335/mahm008887/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/mahm008887/SecondActivity.java
@@ -1,36 +1,99 @@
 package algonquin.cst2335.mahm008887;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import algonquin.cst2335.mahm008887.databinding.ActivitySecondBinding;
 
+//import com.example.FARHANAsAndroidLabs.databinding.ActivitySecondBinding;
+
 public class SecondActivity extends AppCompatActivity {
-ActivitySecondBinding binding;
+
+    ActivitySecondBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivitySecondBinding.inflate(getLayoutInflater());
-        //Loads the XML file /res/layout/activity_second.xml
-        //setContentView(R.layout.activity_second);
+        // Loads the XML file /res/layout/activity_second.xml
         setContentView(binding.getRoot());
-        Intent nextPage = getIntent();//return the intent that got us here.
 
-        //should be variable next page
-        String EMAIL = nextPage.getStringExtra("EMAIL");//get the value of email.
-        int AGE = nextPage.getIntExtra("AGE",0);//get the value of age
-        String day = nextPage.getStringExtra("DAY");
+        Intent nextPage = getIntent();  //return the Intent that got us here
+        //should be variables in nextPage:
 
-        int something = nextPage.getIntExtra("SOMETHING",0);
-        binding.textView3.setText("You passed, "+EMAIL+" and "+ day+" and : "+AGE);
-        binding.goBackButton.setOnClickListener((v)->{
+        //null if EMAIL IS NOT FOUND
+        String EMAIL = nextPage.getStringExtra("EMAIL"); //this will get the value of email
+        String DAY = nextPage.getStringExtra("DAY");
+        int AGE = nextPage.getIntExtra("AGE", 0);
 
-            finish();//go back to previous
+
+        //int something = nextPage.getIntExtra("something", 0);
+
+
+        binding.textView4.setText("You Passed " + EMAIL + " and " + DAY + " and: " + AGE);
+
+        binding.goBackButton.setOnClickListener((v) ->
+        {
+
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            nextPage.putExtra("EmailAddress", binding.editTextPhone.getText().toString());
+            String whatIsTyped = binding.editTextPhone.getText().toString();
+            intent.setData(Uri.parse("tel: " + whatIsTyped));
+            startActivity(intent);
+
+        });
+
+
+        ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+
+                new ActivityResultCallback<ActivityResult>() {
+
+                    @Override
+
+                    public void onActivityResult(ActivityResult result) {
+
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+
+                            Intent data = result.getData();
+                            Bitmap thumbnail = data.getParcelableExtra("data");
+                            binding.imageView.setImageBitmap(thumbnail);
+
+                        }
+                        ;
+                    }
                 });
 
+        binding.button.setOnClickListener(cl -> {
 
+
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            if (checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+                cameraResult.launch(cameraIntent);
+            else
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, 20);
+
+
+//        binding.goBackButton.setOnClickListener((v) ->
+//        {
+//            //opposite of startActivity();
+//            finish(); //go back to previous
+        });
     }
 }
+
+
